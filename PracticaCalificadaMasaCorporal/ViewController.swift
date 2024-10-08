8c9df2567ef7a83aa34a475941499bef441ca9d0
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-10-07 21:48:09 -0500

    Vista 1 y vista 2 actualizado

--- PracticaCalificadaMasaCorporal/ViewController.swift
+++ PracticaCalificadaMasaCorporal/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtPeso: UITextField!
     @IBOutlet weak var txtAltura: UITextField!
     @IBOutlet weak var calcularButton: UIButton!
+    @IBOutlet weak var btnCalcularEnMismaVista: UIButton!
+    @IBOutlet weak var txtResultado: UILabel!
+    @IBOutlet weak var txtInterpretacion: UILabel!
     
     var imcCalculado: Double?
 
@@ -26,10 +29,58 @@ class ViewController: UIViewController {
         calcularButton.backgroundColor = UIColor.systemBlue
         calcularButton.setTitleColor(.white, for: .normal)
         
+        btnCalcularEnMismaVista.layer.cornerRadius = 10
+        btnCalcularEnMismaVista.backgroundColor = UIColor.systemGreen
+        btnCalcularEnMismaVista.setTitleColor(.white, for: .normal)
+        
+        // Ocultar los resultados inicialmente
+        txtResultado.isHidden = true
+        txtInterpretacion.isHidden = true
+
         // Color de fondo
         self.view.backgroundColor = UIColor.systemGray6
     }
+    
+    @IBAction func calcularEnMismaVista(_ sender: Any) {
+        // Cálculo del IMC y mostrar en la misma vista
+        guard let pesoText = txtPeso.text, let peso = Double(pesoText),
+              let alturaText = txtAltura.text, let alturaEnCm = Double(alturaText) else {
+            mostrarAlerta()
+            return
+        }
 
+        let altura = alturaEnCm / 100.0
+        imcCalculado = peso / (altura * altura)
+
+        // Mostramos el resultado en la misma vista
+        if let imcValor = imcCalculado {
+            txtResultado.text = String(format: "Tu IMC es %.2f", imcValor)
+            txtResultado.isHidden = false
+
+            // Clasificación del IMC
+            var interpretacion = ""
+            var colorInterpretacion = UIColor.black
+
+            if imcValor < 18.5 {
+                interpretacion = "Bajo peso. Es recomendable consultar con un médico o nutricionista para mejorar tu salud."
+                colorInterpretacion = UIColor.blue
+            } else if imcValor < 24.9 {
+                interpretacion = "Peso normal. ¡Sigue manteniendo un estilo de vida saludable!"
+                colorInterpretacion = UIColor.green
+            } else if imcValor < 29.9 {
+                interpretacion = "Sobrepeso. Considera realizar actividad física y llevar una dieta equilibrada."
+                colorInterpretacion = UIColor.orange
+            } else {
+                interpretacion = "Obesidad. Te sugerimos hablar con un especialista en salud para reducir riesgos a largo plazo."
+                colorInterpretacion = UIColor.red
+            }
+
+            txtInterpretacion.text = interpretacion
+            txtInterpretacion.textColor = colorInterpretacion
+            txtInterpretacion.isHidden = false
+        }
+    }
+    
     @IBAction func calcularICM(_ sender: Any) {
         guard let pesoText = txtPeso.text, let peso = Double(pesoText),
               let alturaText = txtAltura.text, let alturaEnCm = Double(alturaText) else {
